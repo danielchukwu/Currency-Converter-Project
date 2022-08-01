c678a68e08c93970e2d7701df9acd65b77767b36
Daniel Chukwu <00danzy@gmail.com>
2022-08-01 16:25:30 +0100

    comment code

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -7,50 +7,58 @@ import java.util.ArrayList;
 
 public class CurrencyConverter {
 
+    // First Page
     void convertCurrency() {
-        // Pick Naira to Dollar or dollar to Naira
+        // Pick Naira to Dollar or dollar to Naira (n2d or d2n)
 
-        // Create JFrame
+        // Create JFrame, setSize, setLayout
         JFrame frame = new JFrame("Converter");
         frame.setSize(400, 200);
         frame.setLayout(new GridLayout(1, 2));
 
-        // 2 Buttons
+        // Create and add buttons to frame
         JButton n2dButton = new JButton("Naira to Dollar");
         JButton d2nButton = new JButton("Dollar to Naira");
         frame.add(n2dButton);
         frame.add(d2nButton);
 
-        frame.setVisible(true);
+        frame.setVisible(true);  // set frame to visible
 
+        // Set listener for first button
         n2dButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.setVisible(false);
-                enterCurrencyForConversion("n2d");
+                enterCurrencyForConversion("n2d");    // if Dollar to naira button clicked on call enterCurrencyForConversion method(in line 47) with n2d
             }
         });
 
+        // Set listener for first button
         d2nButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.setVisible(false);
-                enterCurrencyForConversion("d2n");
+                enterCurrencyForConversion("d2n");     // if Dollar to naira button clicked on call enterCurrencyForConversion method(in line 47) with d2n
             }
         });
     }
 
+    // Second Page
     void enterCurrencyForConversion(String typeOfConversion) {
         // Create JFrame
         JFrame frame = new JFrame("Converter");
         frame.setSize(400, 200);
         frame.setLayout(new GridLayout(2, 1));
 
+        // create TextField to recieve data from user for conversion
         JTextField input = new JTextField();
-        frame.add(input);
+        frame.add(input);    // add input to frame
         JButton button = new JButton("convert");
-        frame.add(button);
+        frame.add(button);   // add button to frame
 
+        frame.setVisible(true);  // set frame to visible
+
+        // add event listener on button. when button is clicked execute the below block of code
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,10 +66,13 @@ public class CurrencyConverter {
                 float inputNumber = Integer.parseInt(inputText);
                 double money = 0.0;
 
+                // if n2d convert input from naira to dollar
                 if (typeOfConversion == "n2d") {
                     money = inputNumber / 700;
 
                     System.out.println("Thats " + money + " Dollars");
+
+                // if n2d convert input from dollar to naira
                 } else if (typeOfConversion == "d2n") {
                     money = inputNumber * 700;
 
@@ -70,17 +81,14 @@ public class CurrencyConverter {
 
                 // write to file
                 try {
-                    FileWriter file = new FileWriter("file.txt");
-                    file.write("Your Conversion " + typeOfConversion + " is " + money );
-                    file.close();
+                    FileWriter file = new FileWriter("file.txt");      // create file
+                    file.write("Your Conversion " + typeOfConversion + " is " + money );       // write to file
+                    file.close();  // close file
                 } catch (Exception anError){
                     System.out.println(anError);
                 }
             }
         });
-
-
-        frame.setVisible(true);
     }
 
     void convertAndWrite() {
